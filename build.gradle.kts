b0d4097e93cbcc759f615d48569e2ec843d1e4cc
hyeongmin <nsby8882@naver.com>
2025-08-24 16:23:10 +0900

    feat: remove developmentOnly dependency for spring-boot-devtools

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
